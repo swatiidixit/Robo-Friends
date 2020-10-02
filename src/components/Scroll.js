964b3c8e7c7891a48ebbb5766dedccc46b36206f
bayek0fsiwa <imvikas7526@gmail.com>
2020-10-02 09:10:50 +0530

    updated scrollbar js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,8 +3,9 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-		<div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
+		<div class="scrollbar" id="style-4" style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
 		 {props.children}
+		 <div class="force-overflow"></div>
 		 </div>
 		 );
 };
